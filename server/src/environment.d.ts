779d0d695d8980c5dc6248aa068b0755d0563761
PB-VT <98140708+JuanP-VT@users.noreply.github.com>
2023-10-03 14:33:00 -0700

    UPD Comments

--- server/src/environment.d.ts
+++ server/src/environment.d.ts
@@ -1,9 +1,15 @@
+// This module is empty and is used to indicate that this is a module file.
 export {};
-
+// Declare global types for Node.js environment variables
 declare global {
+  // Extend the NodeJS namespace to include a ProcessEnv interface
   namespace NodeJS {
+    // The ProcessEnv interface defines the types for environment variables
     interface ProcessEnv {
-      DB_PORT: String;
+      // port for database connections
+      DB_PORT: string;
+      // URL for connecting to MongoDB
+      MONGO_URL: string;
     }
   }
 }
