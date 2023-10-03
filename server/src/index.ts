1f0ec3349e5135dba95104c3a7655078ff1bf649
PB-VT <98140708+JuanP-VT@users.noreply.github.com>
2023-10-03 14:34:53 -0700

    Stablish mongodb connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
 //dotenv file
 dotenv.config();
+//mongodb connection
+mongoose.connect(process.env.MONGO_URL);
 
 const app: Application = express();
 const port = process.env.DB_PORT || 8800;
+
+//setup JSON
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + Typescript server");
