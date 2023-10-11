b4d511e90f094da46af6fa41c52fbbf04abf6e6d
PB-VT <98140708+JuanP-VT@users.noreply.github.com>
2023-10-11 16:14:44 -0700

    UPD categories backend service

--- server/src/routes/categoriesRoute.ts
+++ server/src/routes/categoriesRoute.ts
@@ -2,27 +2,27 @@
   This module defines an Express route for managing product categories.
   It provides endpoints to fetch all categories and create new ones.
   The code also includes data validation and error handling for each route.
+
+  Possible status : 200, 500, 400, 409 , 201,
 */
 import express from "express";
 import { categoryModel } from "../Models/category";
 import productCategory from "../factory/product-category";
 import { productCategoryValidationSchema } from "../validation/productCategory";
-
 const categoriesRoute = express.Router();
-
 // Route: Get all categories
 categoriesRoute.get("/", async (req, res) => {
   try {
     // Fetch all categories from the database
     const allCategories = await categoryModel.find();
-    res.status(200).json(allCategories); // Respond with a JSON array of categories
+    res.json(allCategories); // Respond with a JSON array of categories
   } catch (err) {
     // Handle any errors and respond with a 500 Internal Server Error
     res.status(500).json({ error: err });
   }
 });
 
-// Route: Create a new category
+// Route: Create a new product category
 categoriesRoute.post("/", async (req, res) => {
   try {
     // Extract data from the request body
@@ -36,18 +36,22 @@ categoriesRoute.post("/", async (req, res) => {
     const validation =
       productCategoryValidationSchema.validate(newProductCategory);
 
-    if (validation.error) {
-      // Respond with a 400 Bad Request status code and validation error details
-      res.status(400).json({ error: validation.error.details });
+    // Convert validation error details to an array of error messages
+    const errorMessages = validation.error?.details.map(
+      (detail) => detail.message
+    );
+
+    // if validation fails send error details
+    if (errorMessages !== undefined && errorMessages?.length > 0) {
+      res.json({ message: errorMessages[0] });
       return;
     }
 
     // Check for duplicate category name in the database
     const findDuplicate = await categoryModel.findOne({ name });
-
     if (findDuplicate) {
       // Respond with a 409 Conflict status code for duplicate entries
-      res.status(409).send("Duplicate category name");
+      res.json({ message: "Category name must be unique" });
       return;
     }
 
@@ -56,10 +60,11 @@ categoriesRoute.post("/", async (req, res) => {
     await newCategoryInstance.save();
 
     // Respond with a 201 Created status code for successful creation
-    res.status(201).send("Success");
+    res.status(201).json({ data: newCategoryInstance });
   } catch (err) {
     // Handle any errors and respond with a 500 Internal Server Error
-    res.status(500).json({ error: err });
+    console.log(err);
+    res.status(500);
   }
 });
 
