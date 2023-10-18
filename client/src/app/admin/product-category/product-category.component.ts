1b88b656a35d1134c589377b36df5a544fd6b3dd
Juan Pablo <cv.juanp@gmail.com>
2023-10-17 22:15:52 -0700

    ADD product-category-component

--- client/src/app/admin/product-category/product-category.component.ts
+++ client/src/app/admin/product-category/product-category.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProductCategoryService } from 'src/app/shared/services/product-category.service';
+import { ProductCategory } from 'src/app/shared/types/product-category';
 @Component({
   selector: 'app-product-category',
   templateUrl: './product-category.component.html',
-  styleUrls: ['./product-category.component.css']
+  styleUrls: ['./product-category.component.css'],
 })
-export class ProductCategoryComponent {
-
+export class ProductCategoryComponent implements OnInit {
+  constructor(public productCategoryService: ProductCategoryService) {}
+  categoryList: ProductCategory[] = [];
+  ngOnInit(): void {
+    this.productCategoryService.getCategories().subscribe((response) => {
+      if (response.data) {
+        this.categoryList = response.data;
+      }
+    });
+  }
 }
